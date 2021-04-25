e897df7ebf7f3b418043378001d5950c6a330aa6
Danil Bugaenko <danil.bugaenko@mail.ru>
2021-04-25 14:42:19 +0200

    bugfix: js margins

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,19 @@
 let openPopupButton = document.querySelector('.profile__edit-button');
 let closePopupButton = document.querySelector('.popup__button-close');
-let nameInput = document.querySelector('.input__text_type_name');
-let jobInput = document.querySelector('.input__text_type_description');
+let nameInput = document.querySelector('.form__input_type_name');
+let jobInput = document.querySelector('.form__input_type_description');
 let profileName = document.querySelector('.profile__name');
 let profileText = document.querySelector('.profile__text');
-let formButton = document.querySelector('.popup__button-save');
 let popup = document.querySelector('.popup');
+let popupForm = document.querySelector('.form');
 
-function openPopup(evt) {
+function openPopup() {
 	popup.classList.add('popup_opened');
 	nameInput.value = profileName.textContent;
 	jobInput.value = profileText.textContent;
 }
 
-function closePopup(evt) {
+function closePopup() {
 	popup.classList.remove('popup_opened');
 }
 
@@ -25,10 +25,10 @@ function formSubmitHandler(evt) {
 }
 
 //обработчик кнопки сохранения(отправки формы)
-formButton.addEventListener("click", formSubmitHandler); 
+popupForm.addEventListener('submit', formSubmitHandler); 
 
 //обработчик кнопки открытия ПопАпа(изменения профиля)
-openPopupButton.addEventListener("click", openPopup);
+openPopupButton.addEventListener('click', openPopup);
 
 //обработчик кнопки закрытия попапа
-closePopupButton.addEventListener("click", closePopup);
+closePopupButton.addEventListener('click', closePopup);
